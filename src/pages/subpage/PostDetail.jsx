17d70ee81533d079b8de16348d63ff811635ebe4
soljiiii <ggg454538@gmail.com>
2024-06-27 22:27:48 +0900

    글 상세보기 구현

--- src/pages/subpage/PostDetail.jsx
+++ src/pages/subpage/PostDetail.jsx
@@ -1,17 +1,33 @@
 import Header from "../../layouts/Header"
 import replyComponent from "../../component/community/replyComponent"
 import "../../styles/Community.css"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { formatDate } from "../../util/util";
+import axios from "axios"
 
 function PostDetail(){
 
     const {postNo} = useParams();
+    const [detail, setDetail] = useState([]);
 
     //렌더링할때 글 불러오기
     useEffect(()=>{
-        
+        axios.get(`http://localhost:80/postDetail/${postNo}`)
+        .then(response=>{
+            console.log(response.data)
+            setDetail(response.data)
+        })
     },[])
+
+    function postDate(dateString){
+        const date = new Date(dateString);
+        const year = date.getFullYear;
+        const month = String(date.getMonth()+1).padStart(2,'0');
+        const day = String(date.getDate()).padStart(2,'0');
+        
+        return `${year}-${month}-${day}`;
+    }
 
     return(
         <>
@@ -19,16 +35,16 @@ function PostDetail(){
             <div className="postDetailContainer">
                 <div className="postDetailBox">
                     <div className="postDetailTitleBox">
-                        글제목
+                        {detail.title}
                     </div>
                     <div className="postDetailStateBox">
-                        <div className="postDetailVwCntBox">조회수</div>
-                        <div className="postDetailDateBox">날짜</div>
-                        <div className="postDetailIdBox">아이디</div>
+                        <div className="postDetailVwCntBox">{detail.vwCnt}</div>
+                        <div className="postDetailDateBox">{formatDate(detail.wrtnDate)}</div>
+                        <div className="postDetailIdBox">{detail.memberId}</div>
                     </div>
                     <div className="postDetailContentBox">
                         <div className="contentBox">
-                            글내용
+                            {detail.postContent}
                         </div>
                         <div className="likeButtonBox">
                             <button className="likeButton">좋아요</button>
